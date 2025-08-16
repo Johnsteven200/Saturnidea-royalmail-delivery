b99c8c95a937eb7d59c3de2ebbfc6b653acd9b00
Ceng yung <112200829+Johnsteven200@users.noreply.github.com>
2025-08-16 14:18:25 +0100

    Update 27584844LXVN.js

--- 27584844LXVN.js
+++ 27584844LXVN.js
@@ -1,17 +1,17 @@
 function handle27584844LXVN(resultElement) {
     let result = {
         sender: 'Dae bong',
-        country: 'Scotland',
+        country: 'dubai',
         receiver: 'EMMA Ortiz ci',
-        date: 'Jan 19, 2024',
+        date: 'Jan 16, 2025',
         location: 'Out for Delivery - Out for Delivery - Area. Within 24 hours package would get to destined country - pending payment of $150'
     };
 
     // Display the result
     resultElement.innerHTML = `
-        <p>SENDER NAME: ${result.sender}</p>
-        <p>SENDER ADDRESS: ${result.country}</p>
-        <p>RECIEVERS NAME : ${result.receiver}</p>
+        <p>SENDER NAME:lee ${result.sender}</p>
+        <p>SENDER ADDRESS:chang ${result.country}</p>
+        <p>RECIEVERS NAME piro: ${result.receiver}</p>
         <p>DATE: ${result.date}</p>
         <p><video id="local" 
         src="live.mp4" autoplay muted loop></video>           Live Location: ${result.location}</p>`;
