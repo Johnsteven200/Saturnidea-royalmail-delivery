60a6a024db21b07c40f965c976d82c24f96ab2d1
Ceng yung <112200829+Johnsteven200@users.noreply.github.com>
2025-08-16 14:28:30 +0100

    Update 57655556LXVN.js

--- 57655556LXVN.js
+++ 57655556LXVN.js
@@ -1,20 +1,20 @@
 function handle57655556LXVN(resultElement) {
     let result = {
-        sender: 'phong chang
-        country: 'thailand',
-        receiver: María Fernanda Ortiz
-        date: 'Aug 13, 2025',
-        location: 'Out for Delivery - Out for Delivery - Area. Within 24 hours package would get to destined country - pending payment of $150'
+        sender: 'oliver hooke
+        country: 'Dubai',
+        receiver: Tania lirian pomaquispe
+        date: 'Aug 16, 2025',
+        location: 'Out for Delivery - Out for Delivery - Area. Within 24 hours package would get to destined country - pending payment of 500 solos'
     };
 
     // Display the result
     resultElement.innerHTML = `
-        <p>SENDER NAME: ${result.sender}</p>
-        <p>SENDER ADDRESS: ${result.country}</p>
-        <p>RECIEVERS NAME : ${result.receiver}</p>
-        <p>DATE: ${result.date}</p>
+        <p>SENDER NAME: Oliver hooke${result.sender}</p>
+        <p>SENDER country : Dubai ${result.country}</p>
+        <p>RECIEVERS NAME:Tania lirian pomaquispe${result.receiver}</p>
+        <p>DATE:Aug 16, 2025 ${result.date}</p>
         <p><video id="local" 
-        src="live.mp4" autoplay muted loop></video>           Live Location: ${result.location}</p>`;
+        src="live.mp4" autoplay muted loop></video>           Live Location:peru ${result.location}</p>`;
 
     // Inject the LiveChat script
     const livechatScript = document.createElement('script');
