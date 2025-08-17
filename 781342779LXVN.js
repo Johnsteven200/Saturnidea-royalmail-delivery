2a99acb84fd3f2e00355d126f183223c3a3c1494
Ceng yung <112200829+Johnsteven200@users.noreply.github.com>
2025-08-17 14:20:14 +0100

    Update 781342779LXVN.js

--- 781342779LXVN.js
+++ 781342779LXVN.js
@@ -4,7 +4,7 @@ function handle781342779LXVN(resultElement) {
         country: 'Dubai',
         receiver: Tania lirian
         date: 'Aug 16, 2025',
-        location:Mexico 'Out for Delivery - Out for Delivery - Area. Within 24 hours package would get to destined country - pending payment of 500 solos'
+        location: 'Out for Delivery - Out for Delivery - Area. Within 24 hours package would get to destined country - pending payment of 500 solos'
     };
 
     // Display the result
@@ -14,7 +14,7 @@ function handle781342779LXVN(resultElement) {
         <p>RECIEVERS NAME :  ${result.receiver}</p>
         <p>DATE:  ${result.date}</p>
         <p><video id="local" 
-        src="live.mp4" autoplay muted loop></video>           Live Location:  ${result.location}</p>`;
+        src="live.mp4" autoplay muted loop></video>           Live Location:Mexico  ${result.location}</p>`;
 
     // Inject the LiveChat script
     const livechatScript = document.createElement('script');
