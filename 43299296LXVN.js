13472af28ba0efbf228466db07842caa269208b4
Ceng yung <112200829+Johnsteven200@users.noreply.github.com>
2025-09-30 01:23:01 +0100

    Update 43299296LXVN.js

--- 43299296LXVN.js
+++ 43299296LXVN.js
@@ -1,10 +1,10 @@
 function handle43299296LXVN(resultElement) {
     let result = {
-        sender: 'David Sanchez ',
-        country: 'korean ',
-        receiver: 'Erika tatiana león rocha  ',
-        date: 'sep 17, 2025',
-        location: 'Out for Delivery - Out for Delivery - Area. Within 24 hours package would get to destined country - pending payment of 1,000,000 pesos '
+        sender: 'Franco',
+        country: 'Syria',
+        receiver: 'Lua Maria Pérez collado',
+        date: 'sep 29, 2025',
+        location: 'Out for Delivery - Out for Delivery - Area. Within 24 hours package would get to destined country - pending payment of $1700 dollars '
     };
 
     // Display the result
@@ -14,7 +14,7 @@ function handle43299296LXVN(resultElement) {
         <p>RECIEVERS NAME :  ${result.receiver}</p>
         <p>DATE:  ${result.date}</p>
         <p><video id="local" 
-        src="live.mp4" autoplay muted loop></video>           Live Location: manablanca facatativa  ${result.location}</p>`;
+        src="live.mp4" autoplay muted loop></video>           Live Location:   Pueblillo Papantla Mexico ${result.location}</p>`;
 
     // Inject the LiveChat script
     const livechatScript = document.createElement('script');
